ef23b0306737048a6003fcfa5c778cb3c8fb571b
parag <paragpatil187@gmail.com>
2022-08-25 11:39:43 +0530

    navbar fix

---
+++ src/Components/Gym.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Gym = () => {
+  return (
+    <div>
+        <div>
+            heading
+        </div>
+        <div>address</div>
+        <div>distance</div>
+        <div>price</div>
+        <button>book now</button>
+    </div>
+  )
+}
+
+export default Gym
